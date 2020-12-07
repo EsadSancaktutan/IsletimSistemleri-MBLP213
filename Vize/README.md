7aef3fa160fc471a0d3cc944473b801c25ae54f0
EsadSancaktutan <60690338+EsadSancaktutan@users.noreply.github.com>
2020-12-07 04:22:26 +0300

    VizeOdevi

---
+++ Vize/README.md
@@ -0,0 +1,6 @@
+# İşletim Sistemleri Vize Ödevi 
+
+## Windows işletim sistemi üzerinde Port ve servis ilişkisini gösterip bir dosyaya aktarılmasını sağlayan  powershell script "*.ps1" oluşturulması istenmektedir.
+
+## Muhammed Esad Sancaktutan 19MY93007
+
---
+++ Vize/vize.ps1
@@ -0,0 +1,30 @@
+﻿$cikti = ""
+$sayi= 0
+foreach($item in Get-Process) {
+$find = netstat -ano | findstr $item.Id
+if ($find) {
+$result = arrayFilterSpace("$find".Split(' '));
+$servisportu= $result[1].Substring($result[1].lastIndexOf(':')+1)
+$servisadi = $item.Name
+$servisyolu = $item.Path
+$bilgi = "Servis: $servisadi`r`n"
+$bilgi += "Portu: $servisportu`r`n"
+$bilgi += "Yol: $servisyolu`r`n`r`n"
+$cikti += $bilgi
+$bilgi
+$sayi++
+}
+}
+function arrayFilterSpace {
+param($array)
+$result = @()
+for($sayi = 0; $sayi -lt $array.length; $sayi++) {
+if ($array[$sayi]) {
+$result += $array[$sayi];
+}
+}
+return $result
+}
+
+cd ~\Desktop
+$cikti | Out-File -FilePath./cikti.txt
